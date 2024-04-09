d1f8df62000cb49972519789bffd16886488edd9
Kevin <klattkev@gmail.com>
2024-04-09 11:07:00 +0200

    v2

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,14 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
+    finish_add(a, b)
+}
+
+fn finish_add(a: i32, b: i32) -> i32 {
     return a + b;
+}
+
+#[wasm_bindgen]
+pub fn multiply(a: i32, b: i32) -> i32 {
+    return a * b;
 }
